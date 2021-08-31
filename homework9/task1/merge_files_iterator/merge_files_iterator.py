87eb9c5954c58902c98217b58e48db332a2785bb
doppelmarker <doppelmarker@gmail.com>
2021-08-31 20:51:15 +0300

    changed doc to proper one

--- homework9/task1/merge_files_iterator/merge_files_iterator.py
+++ homework9/task1/merge_files_iterator/merge_files_iterator.py
@@ -2,11 +2,11 @@
 Write a function that merges integer from sorted files and returns an iterator
 file1.txt:
 1
-3
-5
-file2.txt:
 2
+3
+file2.txt:
 4
+5
 6
 >>> list(merge_sorted_files(["file1.txt", "file2.txt"]))
 [1, 2, 3, 4, 5, 6]
